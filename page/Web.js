96e38c36120f6d1a977edc20f74c688e048318ac
Soab42 <108150913+Soab42@users.noreply.github.com>
2023-01-19 01:48:44 +0600

    final

--- page/Web.js
+++ page/Web.js
@@ -5,7 +5,7 @@ import { WebView } from "react-native-webview";
 const Web = () => {
   return (
     <View style={{ width: "100%", height: "100%" }}>
-      <WebView source={{ uri: "https://loancalcall.web.app" }} />
+      <WebView source={{ uri: "https://loancalcall.web.app/old" }} />
     </View>
   );
 };
